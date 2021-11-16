1fe44bb2515d184c204d1cd8b88bf98014b8487f
Radu GitHub <79966181+Radu-Robu@users.noreply.github.com>
2021-11-16 20:49:58 +0100

    ENTREGA LISTA 16/11/2021

--- Practica1/P1_Radu_Constantin_Alberto_Perez/1-instalar-dependencias.sh
+++ Practica1/P1_Radu_Constantin_Alberto_Perez/1-instalar-dependencias.sh
@@ -1,14 +1,49 @@
 #!/bin/bash
 
-libreria=$(dpkg -l | grep libcob)
+libreria=$(ldconfig -p | grep libcob.so.4)
 
-if [ "$libreria" == "" ]
+if [ -z "$libreria" ]
 then
   echo "Falta la librería libcob4 para la ejecución del cajero."
   echo -e "Procediendo a instalar la librería libcob4.\n"
   
   sudo apt install libcob4 -y
-  echo -e "\nLibrería instalada. Se puede ejecutar el cajero.\n"
+
+  libreria_instaladas=$(ldconfig -p | grep libcob.so.4)
+
+  if [ -z "$libreria_instaladas" ]
+  then
+    echo -e "Ha habido un error durante la instalación de la librería.\n"
+    echo -e "Se intentará actualizar el repositorio de librerías.\n"
+    sudo apt update -y
+    sudo apt install libcob4 -y
+    
+    libreria_libcob=$(ldconfig -p | grep libcob.so.4)
+    
+    if [ -z "$libreria_libcob" ]
+    then
+      echo -e "\nError debido a una versión desactualizada del sistema operativo.\n"
+      wget http://archive.ubuntu.com/ubuntu/pool/main/n/ncurses/libtinfo6_6.2-0ubuntu2_amd64.deb
+      sudo dpkg -i libtinfo6_6.2-0ubuntu2_amd64.deb
+      
+      wget http://archive.ubuntu.com/ubuntu/pool/main/n/ncurses/libncursesw6_6.2-0ubuntu2_amd64.deb
+      sudo dpkg -i libncursesw6_6.2-0ubuntu2_amd64.deb
+      
+      wget http://archive.ubuntu.com/ubuntu/pool/universe/g/gnucobol/libcob4_2.2-5_amd64.deb
+      sudo dpkg -i libcob4_2.2-5_amd64.deb
+      
+      librerias=$(ldconfig -p | libcob.so.4)
+      
+      if [ -z "$librerias" ]
+      then
+        echo -e "\nLibrerías instaladas. Se puede ejecutar el cajero con \"./cajero\"\n"
+      else
+        echo -e "\nError no contemplado durante la instalación de las librerías.\n"
+      fi
+    fi
+  else 
+    echo -e "\nLibrerías instaladas. Se puede ejecutar el cajero con \"./cajero\"\n"
+  fi
 else
   echo -e "Todas las dependencias encontradas. Se puede ejecutar el cajero.\n"
 fi
