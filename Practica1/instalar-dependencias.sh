a61691f7463235192ecbea23555875d0fbce505a
Radu GitHub <79966181+Radu-Robu@users.noreply.github.com>
2021-11-16 20:35:20 +0100

    Otra actualizacion libcob

--- Practica1/instalar-dependencias.sh
+++ Practica1/instalar-dependencias.sh
@@ -9,18 +9,18 @@ then
   
   sudo apt install libcob4 -y
 
-  libreria=$(ldconfig -p | grep libcob.so.4)
+  libreria_instaladas=$(ldconfig -p | grep libcob.so.4)
 
-  if [ -z "$libreria" ]
+  if [ -z "$libreria_instaladas" ]
   then
     echo -e "Ha habido un error durante la instalación de la librería.\n"
     echo -e "Se intentará actualizar el repositorio de librerías.\n"
     sudo apt update -y
     sudo apt install libcob4 -y
     
-    libreria=$(ldconfig -p | grep libcob.so.4)
+    libreria_libcob=$(ldconfig -p | grep libcob.so.4)
     
-    if [ -z "$libreria" ]
+    if [ -z "$libreria_libcob" ]
     then
       echo -e "\nError debido a una versión desactualizada del sistema operativo.\n"
       wget http://archive.ubuntu.com/ubuntu/pool/main/n/ncurses/libtinfo6_6.2-0ubuntu2_amd64.deb
@@ -32,9 +32,9 @@ then
       wget http://archive.ubuntu.com/ubuntu/pool/universe/g/gnucobol/libcob4_2.2-5_amd64.deb
       sudo dpkg -i libcob4_2.2-5_amd64.deb
       
-      libreria=$(ldconfig -p | libcob.so.4)
+      librerias=$(ldconfig -p | libcob.so.4)
       
-      if [ -z "$libreria" ]
+      if [ -z "$librerias" ]
       then
         echo -e "\nLibrerías instaladas. Se puede ejecutar el cajero con \"./cajero\"\n"
       else
