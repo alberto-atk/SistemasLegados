cab8246df2b04c0edd7b4aedec157d367666fdcd
Radu GitHub <79966181+Radu-Robu@users.noreply.github.com>
2021-11-16 19:27:55 +0100

    Actualizado el fichero de instalación

--- Practica1/instalar-dependencias.sh
+++ Practica1/instalar-dependencias.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
-libreria=$(dpkg -l | grep libcob)
+libreria=$(ldconfig -p | grep libcob)
 
-if [ "$libreria" == "" ]
+if [ -z "$libreria" ]
 then
   echo "Falta la librería libcob4 para la ejecución del cajero."
   echo -e "Procediendo a instalar la librería libcob4.\n"
   
   sudo apt install libcob4 -y
-  echo -e "\nLibrería instalada. Se puede ejecutar el cajero.\n"
+
+  libreria=$(ldconfig -p | grep libcob)
+
+  if [ -z "$libreria" ]
+  then
+    echo -e "Ha habido un error durante la instalación de la librería.\n"
+    echo -e "Se intentará actualizar el repositorio de librerías.\n"
+    sudo apt update -y
+    sudo apt install libcob4 -y
+    
+    libreria_libcob=$(ldconfig -p | grep libcob)
+    
+    if [ -z "$libreria_libcob" ]
+    then 
+      echo -e "\nLibrería instalada. Se puede ejecutar el cajero.\n"
+    else
+      echo -e "\nError no contemplado durante la instalación de la librería.\n"
+    fi
+  else 
+    echo -e "\nLibrería instalada. Se puede ejecutar el cajero.\n"
+  fi
 else
   echo -e "Todas las dependencias encontradas. Se puede ejecutar el cajero.\n"
 fi
