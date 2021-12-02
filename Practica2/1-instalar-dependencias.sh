103302e22741e4a3dda55a7feaf0cf5c4b97de7c
Radu GitHub <79966181+Radu-Robu@users.noreply.github.com>
2021-12-02 16:01:07 +0100

    Preparación entrega
    - Metidos los ficheros de la realización de la práctica en un directorio.
    
    - Hecho el script de instalación de dependencias.
    
    - Actualizado el README con versión.

---
+++ Practica2/1-instalar-dependencias.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+libreria=$(sudo dpkg -l | grep libc6-i386)
+
+if [ -z "$libreria" ]
+then
+  echo "Falta la librería libc6-i386 para la ejecución del programa."
+  echo -e "Procediendo a instalar la librería libc6-i386.\n"
+  
+  sudo apt install glibc -y
+  sudo apt install libc6-i386 -y
+
+  libreria_instaladas=$(sudo dpkg -l | grep libc6-i386)
+
+  if [ -z "$libreria_instaladas" ]
+  then
+    echo -e "Ha habido un error durante la instalación de la librería.\n"
+    echo -e "Se intentará actualizar el repositorio de librerías.\n"
+
+    sudo apt update -y
+    sudo apt install glibc -y
+    sudo apt install libc6-i386 -y
+    
+    libreria_libcob=$(sudo dpkg -l | grep libc6-i386)
+    
+    if [ -z "$libreria_libc6" ]
+    then
+      echo -e "\nError debido a una versión desactualizada del sistema operativo.\n"
+
+      wget http://archive.ubuntu.com/ubuntu/pool/main/g/glibc/libc6_2.34-0ubuntu3_amd64.deb
+      sudo dpkg -i libc6_2.34-0ubuntu3_amd64.deb
+
+      wget http://archive.ubuntu.com/ubuntu/pool/main/g/glibc/libc6-i386_2.34-0ubuntu3_amd64.deb
+      sudo dpkg -i libc6-i386_2.34-0ubuntu3_amd64.deb
+      
+      librerias=$(sudo dpkg -l | grep libc6-i386)
+      
+      if [ -z "$librerias" ]
+      then
+        echo -e "\nLibrerías instaladas. Se puede ejecutar el programa legado1.\n"
+      else
+        echo -e "\nError no contemplado durante la instalación de las librerías.\n"
+      fi
+    fi
+  else 
+    echo -e "\nLibrerías instaladas. Se puede ejecutar el programa legado1.\n"
+  fi
+else
+  echo -e "Todas las dependencias encontradas.Se puede ejecutar el programa legado1.\n"
+fi
