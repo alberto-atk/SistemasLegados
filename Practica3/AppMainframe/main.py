9c70a6f4deab0eec4b5b65c660a2459cdcbffa73
Radu Robu <radurobu@protonmail.com>
2021-12-14 20:54:56 +0100

    Conseguido mandar ascii

--- Practica3/AppMainframe/main.py
+++ Practica3/AppMainframe/main.py
@@ -7,7 +7,7 @@ from py3270 import *
 emulator = Emulator()
 emulator.connect("155.210.71.101:123")
 emulator.send_enter()
-emulator.exec_command("ascii()\n")
+print(emulator.exec_command("ascii".encode('ASCII')).data)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
