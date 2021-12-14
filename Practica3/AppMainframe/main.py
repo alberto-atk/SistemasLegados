9b771ab29be7fd651001b0de7e5b520053309d2b
Radu Robu <radurobu@protonmail.com>
2021-12-14 20:51:24 +0100

    Pruebas con py3270

--- Practica3/AppMainframe/main.py
+++ Practica3/AppMainframe/main.py
@@ -2,15 +2,12 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from py3270 import *
 
+emulator = Emulator()
+emulator.connect("155.210.71.101:123")
+emulator.send_enter()
+emulator.exec_command("ascii()\n")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
