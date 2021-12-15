a9fa6adb451c1ba3db28e47950fb012f895c0647
Radu Robu <radurobu@protonmail.com>
2021-12-15 08:47:34 +0100

    Incluidos comentarios explicativos

--- Practica3/AppMainframe/main.py
+++ Practica3/AppMainframe/main.py
@@ -1,13 +1,7 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from py3270 import *
 
 emulator = Emulator()
 emulator.connect("155.210.71.101:123")
 emulator.send_enter()
-print(emulator.exec_command("ascii".encode('ASCII')).data)
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(emulator.exec_command("ascii".encode(
+    'ASCII')).data)  # Muestra en pantalla la información recibida del emulador
