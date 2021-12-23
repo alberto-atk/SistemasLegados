b6ee4934436d7f705c43a9eda3b51abcffcb4c06
Radu Robu <radurobu@protonmail.com>
2021-12-23 16:58:15 +0100

    Avance 23/12
    - Conexión
    - Login
    - Menú principal
    - Logout

--- Practica3/AppMainframe/main.py
+++ Practica3/AppMainframe/main.py
@@ -1,7 +1,51 @@
 from py3270 import *
 
+# Constantes conexión
+MACHINE_IP = "155.210.71.101:"
+MACHINE_PORT = "123"
+USERNAME = "prog"
+PASSWORD = "prog123"
+
+# Constantes comandos
+COMM_TAB = 'Tab'
+COMM_ASCII = 'ascii'
+COMM_OFF = 'off'
+
+# Constantes formatos
+ASCII = 'ASCII'
+
+def show_screen_result():
+    print(emulator.exec_command(COMM_ASCII.encode(ASCII)).data)
+
+def connect():
+    emulator.connect(MACHINE_IP + MACHINE_PORT)
+    emulator.send_enter()
+    emulator.send_enter()
+    emulator.send_enter()
+
+def login():
+    emulator.send_string(USERNAME)
+    emulator.exec_command(COMM_TAB.encode(ASCII))
+    emulator.send_string(PASSWORD)
+    emulator.send_enter()
+    emulator.send_enter()
+    emulator.send_enter()
+    emulator.send_enter()
+    emulator.send_enter()
+    emulator.send_enter()
+
+def logout():
+    emulator.send_string(COMM_OFF)
+    emulator.send_enter()
+    emulator.send_enter()
+    emulator.send_enter()
+    emulator.terminate()
+
 emulator = Emulator()
-emulator.connect("155.210.71.101:123")
-emulator.send_enter()
-print(emulator.exec_command("ascii".encode(
-    'ASCII')).data)  # Muestra en pantalla la información recibida del emulador
+connect()
+login()
+
+show_screen_result()
+logout()
+
+
