f0504e33524386870e073e7327ba020a4dccadb2
alberto-atk <73551392+alberto-atk@users.noreply.github.com>
2021-12-29 11:27:15 +0100

    cambio a espanyol

--- Practica3/AppMainframe/interfazGrafica.py
+++ Practica3/AppMainframe/interfazGrafica.py
@@ -1,26 +1,38 @@
 from tkinter import *
 
-def newTaskFile(window):
+
+TEXTO_BOTON_NUEVO_FICHERO = "Nuevo fichero"
+TEXTO_BOTON_ANYADIR_TAREA = "Añadir tarea"
+TEXTO_BOTON_ELIMINAR_TAREA = "Eliminar tarea"
+TEXTO_BOTON_BUSCAR_TAREA = "Buscar tarea"
+TEXTO_BOTON_LISTAR_TAREAS = "Listar tareas"
+TEXTO_BOTON_GUARDAR = "Guardar"
+TEXTO_BOTON_SALIR = "Salir"
+TITULO_VENTANA = "Task Management"
+TAMANYO_VENTANA = '800x600'
+
+
+def nuevo_fichero(window):
     print("New file")
 
 
-def addTask(window):
+def anyadir_tarea(window):
     print("add ")
 
 
-def removeTask(window):
+def eliminar_tarea(window):
     print("remove")
 
 
-def searchTask(window):
+def buscar_tarea(window):
     print("search")
 
 
-def listTask(window):
+def listar_tareas(window):
     print("list")
 
 
-def saveTask(window):
+def guardar(window):
     print("save")
 
 
@@ -29,26 +41,26 @@ def exit():
 
 
 
-def generarBotones(window):
-    newTaskBtn = Button(window, text="Nuevo fichero", command=lambda:newTaskFile(window))
+def generar_botones(window):
+    newTaskBtn = Button(window, text=TEXTO_BOTON_NUEVO_FICHERO, command=lambda:nuevo_fichero(window))
     newTaskBtn.place(x=15, y=20, width=150, height=40)
 
-    addTaskBtn = Button(window, text="Añadir tarea", command=lambda:addTask(window))
+    addTaskBtn = Button(window, text=TEXTO_BOTON_ANYADIR_TAREA, command=lambda:anyadir_tarea(window))
     addTaskBtn.place(x=15, y=70, width=150, height=40)
 
-    eliminarBtn = Button(window, text="Eliminar tarea", command=lambda:removeTask(window))
+    eliminarBtn = Button(window, text=TEXTO_BOTON_ELIMINAR_TAREA, command=lambda:eliminar_tarea(window))
     eliminarBtn.place(x=15, y=120, width=150, height=40)
 
-    buscarBtn = Button(window, text="Buscar tarea", command=lambda:searchTask(window))
+    buscarBtn = Button(window, text=TEXTO_BOTON_BUSCAR_TAREA, command=lambda:buscar_tarea(window))
     buscarBtn.place(x=15, y=170, width=150, height=40)
 
-    listarBtn = Button(window, text="Listar tareas", command=lambda:listTask(window))
+    listarBtn = Button(window, text=TEXTO_BOTON_LISTAR_TAREAS, command=lambda:listar_tareas(window))
     listarBtn.place(x=15, y=220, width=150, height=40)
     
-    guardarBtn = Button(window, text="Guardar", command=lambda:saveTask(window))
+    guardarBtn = Button(window, text=TEXTO_BOTON_GUARDAR, command=lambda:guardar(window))
     guardarBtn.place(x=15, y=270, width=150, height=40)
 
-    salirBtn = Button(window, text="Salir", command=exit)
+    salirBtn = Button(window, text=TEXTO_BOTON_SALIR, command=exit)
     salirBtn.place(x=15, y=320, width=150, height=40)
 
 
@@ -56,10 +68,10 @@ def generarBotones(window):
 if __name__ == "__main__":
     window = Tk()
 
-    window.title("Task Management")
+    window.title(TITULO_VENTANA)
 
-    window.geometry('800x600')
+    window.geometry(TAMANYO_VENTANA)
 
-    generarBotones(window)
+    generar_botones(window)
 
     window.mainloop()
